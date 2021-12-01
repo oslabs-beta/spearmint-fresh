9e7726d65d4fe79b876f8ec273c430cd508b929a
alanrichardson7 <91553905+alanrichardson7@users.noreply.github.com>
2021-12-01 14:30:32 -0500

    moved App.jsx to correct location

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from './App.module.scss';
+// import './style/app.scss';
+
+
+const App = () => (
+  <div>
+  <h1> Minty Fresh is chillin </h1>
+  <button>Chill</button>
+ </div>
+
+);
+
+
+export default App;
