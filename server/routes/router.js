37821a4e7862bde4b8504b373b1569d8f56cc4da
TERR-inss <robo.terence@gmail.com>
2021-12-02 11:15:21 -0500

    login endpoint tested with postman, functions correctly

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,8 +2,8 @@
 const express = require('express');
 // Import all relevant controller objects equipped with middleware
 const userController = require('../controllers/userController');
-// const cookieController = require('../controllers/cookieController');
-// const sessionController = require('../controllers/sessionController');
+const cookieController = require('../controllers/cookieController');
+const sessionController = require('../controllers/sessionController');
 // const testStateController = require('../controllers/testStateController');
 // const githubController = require('../controllers/githubController');
 
@@ -24,21 +24,21 @@ router.post(
   }
 );
 
-// // Set up route for post requests to /login
-// router.post(
-//   '/login',
-//   // Login middleware checks encrypted credentials
-//   userController.login,
-//   // Cookie middleware to set up a new cookie
-//   cookieController.setSSIDCookie,
-//   // Session middleware to initialize new session
-//   sessionController.startSession,
-//   // Anonymous middleware to send back valid response
-//   (req, res) => {
-//     console.log('ssid:', res.locals.ssid);
-//     res.status(200).json({ ssid: res.locals.ssid });
-//   }
-// );
+// Set up route for post requests to /login
+router.post(
+  '/login',
+  // Login middleware checks encrypted credentials
+  userController.login,
+  // Cookie middleware to set up a new cookie
+  cookieController.setSSIDCookie,
+  // Session middleware to initialize new session
+  sessionController.startSession,
+  // Anonymous middleware to send back valid response
+  (req, res) => {
+    console.log('ssid:', res.locals.ssid);
+    res.status(200).json({ ssid: res.locals.ssid });
+  }
+);
 
 // // Set up route for get requests to /logout
 // router.get(
