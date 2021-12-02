4665a2a3abbe8ddb59b002540dc74db201124783
TERR-inss <robo.terence@gmail.com>
2021-12-01 20:43:02 -0500

    connecting to mongodb

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,10 +2,11 @@
 const express = require('express');
 // Import all relevant controller objects equipped with middleware
 const userController = require('../controllers/userController');
-const cookieController = require('../controllers/cookieController');
-const sessionController = require('../controllers/sessionController');
-const testStateController = require('../controllers/testStateController');
-const githubController = require('../controllers/githubController');
+// const cookieController = require('../controllers/cookieController');
+// const sessionController = require('../controllers/sessionController');
+// const testStateController = require('../controllers/testStateController');
+// const githubController = require('../controllers/githubController');
+
 // Initialize an express router
 const router = express.Router();
 
@@ -23,74 +24,74 @@ router.post(
   }
 );
 
-// Set up route for post requests to /login
-router.post(
-  '/login',
-  // Login middleware checks encrypted credentials
-  userController.login,
-  // Cookie middleware to set up a new cookie
-  cookieController.setSSIDCookie,
-  // Session middleware to initialize new session
-  sessionController.startSession,
-  // Anonymous middleware to send back valid response
-  (req, res) => {
-    console.log('ssid:', res.locals.ssid);
-    res.status(200).json({ ssid: res.locals.ssid });
-  }
-);
+// // Set up route for post requests to /login
+// router.post(
+//   '/login',
+//   // Login middleware checks encrypted credentials
+//   userController.login,
+//   // Cookie middleware to set up a new cookie
+//   cookieController.setSSIDCookie,
+//   // Session middleware to initialize new session
+//   sessionController.startSession,
+//   // Anonymous middleware to send back valid response
+//   (req, res) => {
+//     console.log('ssid:', res.locals.ssid);
+//     res.status(200).json({ ssid: res.locals.ssid });
+//   }
+// );
 
-// Set up route for get requests to /logout
-router.get(
-  '/logout',
-  // Session middleware to end any existing sessions
-  sessionController.endSession,
-  // Anonymous middleware to send back valid response
-  (req, res) => {
-    res.status(200).json('Logged Out Successfully');
-  }
-);
+// // Set up route for get requests to /logout
+// router.get(
+//   '/logout',
+//   // Session middleware to end any existing sessions
+//   sessionController.endSession,
+//   // Anonymous middleware to send back valid response
+//   (req, res) => {
+//     res.status(200).json('Logged Out Successfully');
+//   }
+// );
 
-// Set up route for post requests to /upload
-router.post(
-  '/upload',
-  // Session middleware to check if current user is signed in
-  sessionController.isLoggedIn,
-  // Upload middleware to save passed test object into DB
-  testStateController.upload,
-  // Anonymous middleware to send back valid response
-  (req, res) => {
-    res.status(200).json('Test Uploaded Successfully');
-  }
-);
+// // Set up route for post requests to /upload
+// router.post(
+//   '/upload',
+//   // Session middleware to check if current user is signed in
+//   sessionController.isLoggedIn,
+//   // Upload middleware to save passed test object into DB
+//   testStateController.upload,
+//   // Anonymous middleware to send back valid response
+//   (req, res) => {
+//     res.status(200).json('Test Uploaded Successfully');
+//   }
+// );
 
-// Set up route for get requests to /getTests with type passed as param
-router.get(
-  '/getTests/:testType',
-  // Session middleware to check if current user is signed in
-  sessionController.isLoggedIn,
-  // GetTests middleware to retrieve all saved tests from DB
-  testStateController.getTests,
-  // Anonymous middleware to send back valid response
-  (req, res) => {
-    res.status(200).json(res.locals.tests);
-  }
-);
+// // Set up route for get requests to /getTests with type passed as param
+// router.get(
+//   '/getTests/:testType',
+//   // Session middleware to check if current user is signed in
+//   sessionController.isLoggedIn,
+//   // GetTests middleware to retrieve all saved tests from DB
+//   testStateController.getTests,
+//   // Anonymous middleware to send back valid response
+//   (req, res) => {
+//     res.status(200).json(res.locals.tests);
+//   }
+// );
 
-// Set up route for get requests to /github with account code passed as param
-router.get(
-  '/github/:code',
-  // Github controller to retrieve signin token with API
-  githubController.getToken,
-  // Github middleware to retrieve user object with API
-  githubController.getUser,
-  // Cookie middleware to set up a new cookie
-  cookieController.setSSIDCookie,
-  // Session middleware to check if current user is signed in
-  sessionController.startSession,
-  // Anonymous middleware to send back valid response
-  (req, res) => {
-    res.status(200).json({ ssid: res.locals.userId });
-  }
-);
+// // Set up route for get requests to /github with account code passed as param
+// router.get(
+//   '/github/:code',
+//   // Github controller to retrieve signin token with API
+//   githubController.getToken,
+//   // Github middleware to retrieve user object with API
+//   githubController.getUser,
+//   // Cookie middleware to set up a new cookie
+//   cookieController.setSSIDCookie,
+//   // Session middleware to check if current user is signed in
+//   sessionController.startSession,
+//   // Anonymous middleware to send back valid response
+//   (req, res) => {
+//     res.status(200).json({ ssid: res.locals.userId });
+//   }
+// );
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
-// const router = require('./routes/router');
+const router = require('./routes/router');
 const PORT = 3001;
  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/', (req, res) => {
-    res.status(200)
-        .contentType('text/html')
-        .sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.use('/', router);
 
 // Any other request is caught here
 app.use((req, res) => res.status(404).send('Error 404: No content found'));
