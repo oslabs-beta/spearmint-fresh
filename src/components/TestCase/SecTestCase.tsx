9e53147606b114701dc383407cb19bec40af32c9
TERR-inss <robo.terence@gmail.com>
2021-12-12 14:12:00 -0500

    added buttons to SecTestCase, onClick handler functions

---
+++ src/components/TestCase/SecTestCase.tsx
@@ -0,0 +1,62 @@
+import React, { useContext } from 'react';
+import styles from './TestCase.module.scss';
+import { setTabIndex } from '../../context/actions/globalActions';
+
+import { GlobalContext } from '../../context/reducers/globalReducer';
+
+import { Button, TextField } from '@material-ui/core';
+
+const ipc = require('electron').ipcRenderer;
+const os = require('os');
+
+const SecTestCase = () => {
+
+  const [{ isFileDirectoryOpen }, dispatchToGlobal] = useContext(GlobalContext);
+
+  // Change execute command based on os platform
+  let execute = '\n';
+  if (os.platform() === 'win32') {
+    execute = '\r';
+  }
+
+  // sends user to webpage to authenticate use of snyk in terminal
+  const snykAuth = () => {
+    ipc.send('terminal.toTerm', `snyk auth ${execute}`);
+    dispatchToGlobal(setTabIndex(2));
+  };
+
+  const dependencyTest = () => {
+    ipc.send('terminal.toTerm', `snyk test ${execute}`);
+    dispatchToGlobal(setTabIndex(2));
+  };
+
+  const applicationTest = () => {
+    ipc.send('terminal.toTerm', `snyk code test ${execute}`);
+    dispatchToGlobal(setTabIndex(2));
+  };
+
+  return (
+    <div id={styles.SecTestCase}>
+      <div id="head">
+        <SecTestMenu />
+      </div>
+
+      <section>
+        <Button variant='contained' type='button' id={styles.snyk} onClick={snykAuth}>
+          Authenticate Snyk
+        </Button>
+        <br />
+        <br />
+        <br />
+        <Button variant='outlined' type='button' id={styles.snyk} onClick={dependencyTest}>
+          Test Dependencies
+        </Button>
+        <br />
+        <br />
+        <Button variant='outlined' type='button' id={styles.snyk} onClick={applicationTest}>
+          Test Application
+        </Button>
+      </section>
+    </div>
+  )
+}
